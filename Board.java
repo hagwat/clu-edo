1f213838a6a0f32990ad55b94ef5c147f9096c6c
hagwat <boutsrom@gmail.com>
2016-07-25 15:26:02 +1200

    working on it

--- Board.java
+++ Board.java
@@ -1,5 +1,17 @@
 package game;
 
 public class Board {
-private Tile[][] tiles;
+	private Tile[][] tiles;
+
+	public void display() {
+		for (int i = 0; i < tiles.length; i++) {
+			for (int j = 0; j < tiles[i].length; j++) {
+				System.out.print(tiles[i][j].toString());
+				if (i == tiles.length - 1) {
+					System.out.println("|");
+				}
+			}
+		}
+	}
+
 }
