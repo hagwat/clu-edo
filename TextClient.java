ca96a6db8133f5841a12f96f0963e789fdb76a54
shawmori <shaw.david.mori@gmail.com>
2016-08-04 17:14:49 +1200

    Add files via upload

--- TextClient.java
+++ TextClient.java
@@ -41,16 +41,15 @@ public class TextClient {
 
 	}
 
-
 	public void setPlayers(){
 		List<String> names = getPlayerNames();
 		List<Integer> tokens = getPlayerTokens(names);
 		Queue<Player> players = new LinkedList<Player>();
 		if(names.size()!=tokens.size()){throw new IllegalStateException("Number of players not equal to number of tokens.");}
 		for(int i=0; i<names.size();i++){
-			players.add(new Player(tokens.get(i), names.get(i), game.getBoard()));
+			players.offer(new Player(tokens.get(i), names.get(i), game.getBoard()));
 		}
-		
+	}
 
 	public static List<String> getPlayerNames(){
 		int players = readInt("How many players? (Must be between 3-6)");
@@ -207,7 +206,7 @@ public class TextClient {
 		validPersons.add("The Reverend Green");
 		validPersons.add("Mrs. Peacock");
 		validPersons.add("Professor Plum");
-		if(validPersons.contains(persons.toLowerCase())) return true;
+		if(validPersons.contains(person.toLowerCase())) return true;
 		return false;
 	}
 
