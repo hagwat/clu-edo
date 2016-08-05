01ef02ff0482b378102bba71eae0ca0af0e8cf73
shawmori <shaw.david.mori@gmail.com>
2016-08-05 21:49:11 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,11 +2,16 @@ package game;
 
 import java.util.*;
 
+/**
+ * Represents a player in the game of Cluedo. A player is the user that is inputting actions.
+ *
+ */
 public class Player {
-	private List<Card> hand;
+	
+	private List<Card> hand;	//This player's hand
 	private int character; // Integer refers to character in Characters.txt
-	private String name;
-	private CharacterToken token;
+	private String name;	//This player's name
+	private CharacterToken token;	//This players CharacterToken
 
 	public Player(int characterNum, String playerName, Board board) {
 		this.character = characterNum;
@@ -32,11 +37,20 @@ public class Player {
 	public String toString() {
 		return name;
 	}
-
+	
+	/**
+	 * Returns a boolean on whether the array of moves passed is valid for this player.
+	 * @param moves
+	 * @return
+	 */
 	public boolean validMove(String[] moves) {
 		return token.validMove(moves);
 	}
-
+	
+	/**
+	 * Add Card c to the player's hand
+	 * @param c
+	 */
 	public void addCard(Card c) {
 		hand.add(c);
 	}
