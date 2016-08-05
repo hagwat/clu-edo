d3753065fddc17e91e0a8938dece09b4ec8a352a
shawmori <shaw.david.mori@gmail.com>
2016-08-05 21:48:08 +1200

    Update Locatable.java

--- Locatable.java
+++ Locatable.java
@@ -1,7 +1,13 @@
 package game;
-
+/**
+ * Represents a locatable piece in the game of Cluedo.
+ *
+ */
 public interface Locatable {
-
+	/**
+	 * returns the x and y co-ordinates of a locatable piece
+	 * @return
+	 */
 	public int[] getLocation();
 
 }
