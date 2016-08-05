48c3af727b0eba7ff83c8245dd38ee6a5afce3ff
shawmori <shaw.david.mori@gmail.com>
2016-08-05 18:25:43 +1200

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -3,10 +3,34 @@ package game;
 
 public class Weapon implements Locatable {
 
+	private String name;
+
+	public Weapon(String name){
+		this.name = name;
+	}
+
 	@Override
 	public int[] getLocation() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	public String getName(){
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof Weapon){
+			Weapon w = (Weapon)o;
+			if(w.getName().equalsIgnoreCase(this.getName())){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString(){
+		return this.name;
+	}
 }
