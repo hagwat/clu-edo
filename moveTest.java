b1a5aefe6c2a16a1b281790f39695cb40d822503
hagwat <boutsrom@gmail.com>
2016-08-05 13:31:26 +1200

    Add files via upload

--- moveTest.java
+++ moveTest.java
@@ -1,4 +1,4 @@
-package testcases;
+package tests;
 
 import static org.junit.Assert.*;
 
@@ -17,12 +17,22 @@ public class moveTest {
 	public void test1() {
 		Game game = new Game("test");
 		Player testGuy = new Player(1, "James", game.getBoard());
-		System.out.println(testGuy.getToken());
 		//--------------------------------------------------------|
 		System.out.println(testGuy.validMove(new String[]{"w", "w", "w", "d"}));
-		
-		
-		
+		if(testGuy.getToken().getLocation()[0]!=8||testGuy.getToken().getLocation()[1]!=21){
+			fail("incorrect position");
+		}		
+	}
+	
+	@Test
+	public void test2() {
+		Game game = new Game("test");
+		Player testGuy = new Player(2, "James", game.getBoard());
+		//--------------------------------------------------------|
+		System.out.println(testGuy.validMove(new String[]{"d", "w", "d", "a", "a"}));
+		if(testGuy.getToken().getLocation()[0]!=2||testGuy.getToken().getLocation()[1]!=18){
+			fail("incorrect position");
+		}		
 	}
 
 }
